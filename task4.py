b0fef5e4294a432ab311fdc79591e58ff52ed292
aaryannP <aryanparmar5116@gmail.com>
2025-07-02 18:27:46 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,33 @@
+import random
+
+numbers = []
+while len(numbers) < 12:
+    num = random.randint(1, 100)
+    if num not in numbers:
+        numbers.append(num)
+
+player1 = numbers[:6]
+player2 = numbers[6:]
+
+print("Player 1:", player1)
+print("Player 2:", player2)
+
+
+while len(player1) > 0 and len(player2) > 0:
+    guess = random.choice(numbers)
+    print("Guess:", guess)
+    if guess in player1:
+        player1.remove(guess)
+        numbers.remove(guess)
+        print("Hit Player 1!")
+    elif guess in player2:
+        player2.remove(guess)
+        numbers.remove(guess)
+        print("Hit Player 2!")
+    
+
+
+if len(player1) == 0:
+    print("Player 1 wins!")
+elif len(player2) == 0:
+    print("Player 2 wins!")
