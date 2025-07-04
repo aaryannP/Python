cd0adb6341339a7a4a725fac6e9cbf13861e6b96
aaryannP <aryanparmar5116@gmail.com>
2025-07-04 08:29:06 +0530

    task5.Jaybhavani.py

--- task.Jaybhavani.py
+++ task.Jaybhavani.py
@@ -1,46 +1,4 @@
-                        # jay bhavani 
-
-
-"""
-{"vadapav", 
-    {
-        'qty' : 2,
-        'price' : 20
-    }
-}
-            MENU 
-
-vadapav    50 Rs.
-dabeli     30 Rs.
-sandwich   180 Rs.
-
-Enter product which you want to buy : vadapav 
-Enter no. of qty you want : 3
-
-    price will be ::: 3 X 50Rs. =  150 Rs
-
-do you want to add more product ?  press y for yes : y 
-
-Enter product which you want to buy : dabeli
-Enter no. of qty you want : 2
-
-    price will be ::: 2 X 30Rs. =  60 Rs
-
-do you want to add more product ?  press y for yes : n
-
-INVOICE :
-------------------
-vadapav   Qty. 3 X Rs. 50  = Rs. 150
-dabeli    Qty. 2 X Rs. 30  = Rs.  60
-----------------------------------
-Total : Rs. 210
-
-
-
-"""
-
-
-products = {} # main dictionary 
+products = {} # main 
 status = True 
 menu = """ 
         press 1 for product manager
@@ -48,10 +6,10 @@ menu = """
 """
 
 while status:
-    sub_dict = {} 
+    sub_dict = {} # child
     print(menu)
     choice = int(input("Enter your choice : "))
-    if choice == 1:
+    if choice == 1: # for product manager
         product_name = input("Enter product : ").lower()       
         qty = int(input("Enter qty : "))
         price = int(input("Enter price : "))
@@ -67,15 +25,14 @@ while status:
 
         products[product_name] = sub_dict
 
-        # print(products)
-
         for k in products.keys():
             print(f"{k} Qty. {products[k]['qty']}  Price. {products[k]['price']} Rs.")
 
 
     elif choice == 2:  # for customer 
         total = 0
-        while True:
+        status1 = True
+        while status1:
             print("MENU")
             for k in products.keys():
                 print(f"{k} {products[k]['price']} Rs.")
